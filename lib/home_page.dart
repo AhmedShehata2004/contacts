eed0e2939c48232a7f9448ea30e7a6487cfdfbac
Ahmed Shehata <ahmed.0.shehata.2040@gmail.com>
2024-04-04 05:26:41 +0200

    FINISH ASSIGMENT 3

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:contacts_route/models/contact.dart';
 import 'package:contacts_route/widgets/contact_card.dart';
 import 'package:contacts_route/widgets/custom_textfield.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,7 +9,6 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
 class _HomeScreenState extends State<HomeScreen> {
     List<Contact> contactList=[Contact(),Contact(),Contact()];
    TextEditingController nameController =TextEditingController();
@@ -29,61 +27,75 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body:    SingleChildScrollView(
           child: Column(
-  children: [
-       CustomTextField(
-            controller: nameController,
-            hintText: "Enter Your Name Here", suffixIcon: Icon(Icons.edit)
-       ),
-       CustomTextField(
-             controller: phoneController,
-             hintText: "Enter Your Phone Here", suffixIcon: Icon(Icons.phone)
-       ),
-    const SizedBox(width: 50,),
-
-    Row(
             children: [
-              const SizedBox(width: 10,),
-             Expanded(
-               child: FilledButton(
-                 onPressed: (){
-                    if(counter<3){
-                       contactList[counter]=Contact(
-                      visibility: true,
-                      name:nameController.text ,
-                         phone:phoneController.text,
-
-     );
-             counter++;
-             setState(() {});
-   }
-
-               },
-                   child: Text("Add",style: TextStyle(color: Colors.black),),
-                style: FilledButton.styleFrom(backgroundColor: Colors.blue),
-               ),
+              CustomTextField(
+                  controller: nameController,
+                  hintText: "Enter Your Name Here",
+                  suffixIcon: Icon(Icons.edit)),
+              CustomTextField(
+                  controller: phoneController,
+                  hintText: "Enter Your Phone Here",
+                  suffixIcon: Icon(Icons.phone)),
+              const SizedBox(
+                width: 50,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: FilledButton(
+                      onPressed: () {
+                        if (counter < 3) {
+                          contactList[counter] = Contact(
+                            visibility: true,
+                            name: nameController.text,
+                            phone: phoneController.text,
+                          );
+                          counter++;
+                          nameController.clear();
+                          phoneController.clear();
+                          setState(() {});
+                        }
+                      },
+                      child: Text(
+                        "Add",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style:
+                          FilledButton.styleFrom(backgroundColor: Colors.blue),
+                    ),
              ),
               const SizedBox(width: 10,),
 
               Expanded(
-                child: FilledButton(onPressed: (){},
-                  child: Text("Delete",style: TextStyle(color: Colors.black),),
-                  style: FilledButton.styleFrom(backgroundColor: Colors.red,),
-                ),
+                    child: FilledButton(
+                      onPressed: () {
+                        if (counter > 0) {
+                          contactList[counter - 1] = Contact();
+                          counter--;
+                          setState(() {});
+                        }
+                      },
+                      child: Text(
+                        "Delete",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style: FilledButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                ],
               ),
-              const SizedBox(width: 10,),
-
+              ContactCard(contact: contactList[0]),
+              ContactCard(contact: contactList[1]),
+              ContactCard(contact: contactList[2]),
             ],
-          ),
-
-    // Visibility(
-    //     visible: contactList[0].visibility,
-    //     child: Text(contactList[0].name??"")),
-
-    ContactCard(contact: contactList[0]),
-    ContactCard(contact: contactList[1]),
-    ContactCard(contact: contactList[2]),
-
-  ],
 ),
         ),
       ),
--- lib/widgets/contact_card.dart
+++ lib/widgets/contact_card.dart
@@ -1,27 +1,49 @@
 import 'package:contacts_route/models/contact.dart';
 import 'package:flutter/material.dart';
 
-class ContactCard extends StatelessWidget {
+class ContactCard extends StatefulWidget {
   const ContactCard({super.key, required this.contact});
+
   final Contact contact;
+
+  @override
+  State<ContactCard> createState() => _ContactCardState();
+}
+
+class _ContactCardState extends State<ContactCard> {
+  bool showMore = false;
 
   @override
   Widget build(BuildContext context) {
     return Visibility(
-
+        visible: widget.contact.visibility,
         child: Container(
           width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal:15 ,vertical: 20),
-      margin: const EdgeInsets.symmetric(horizontal:5 ,vertical: 10),
-
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30.0), color: Colors.white),
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0), color: Colors.white),
           child: Column(children: [
-              Text("Name: ${contact.name}",style: TextStyle(fontSize: 18),),
-               const SizedBox(height: 10,),
-               Text("Phone: ${contact.phone}",style: TextStyle(fontSize: 18),),
-
-  ]),
-    )
+            InkWell(
+              onTap: () {
+                showMore = !showMore;
+                setState(() {});
+              },
+              child: Text(
+                "Name: ${widget.contact.name}",
+                maxLines: showMore == false ? 1 : null,
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Phone: ${widget.contact.phone}",
+              style: TextStyle(fontSize: 18),
+            ),
+          ]),
+        )
     );
   }
 }
